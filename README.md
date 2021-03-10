64ba6ab15a26b34f7bf9e0163d9804072d859fd6
BVV-Centos <vblvbl133@gmail.com>
2021-03-10 11:27:36 +0300

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a chaaterbot version for RB.
+Some minior add-ons + local upload of nltk files.
---
+++ preprocessors.py
@@ -0,0 +1,65 @@
+"""
+Statement pre-processors.
+"""
+
+from nltk import RegexpTokenizer
+
+#create tokenizer - remove punctuation:
+sentence = "Think and wonder, wonder and think."
+
+def remove_punctuation(statement):
+    """
+    Remove any punctuation from the statement text
+    """    
+    # Make tokenizer
+    tokenizer = RegexpTokenizer(r"\w+")
+
+    #make a list of words without punctuation
+    temp_text = tokenizer.tokenize(statement.text)
+    statement.text = " ".join(tokenizer.tokenize(statement.text))
+    print(statement.text)
+    return statement
+
+
+def clean_whitespace(statement):
+    """
+    Remove any consecutive whitespace characters from the statement text.
+    """
+    import re
+
+    # Replace linebreaks and tabs with spaces
+    statement.text = statement.text.replace('\n', ' ').replace('\r', ' ').replace('\t', ' ')
+
+    # Remove any leeding or trailing whitespace
+    statement.text = statement.text.strip()
+
+    # Remove consecutive spaces
+    statement.text = re.sub(' +', ' ', statement.text)
+
+    return statement
+
+
+def unescape_html(statement):
+    """
+    Convert escaped html characters into unescaped html characters.
+    For example: "&lt;b&gt;" becomes "<b>".
+    """
+    import html
+
+    statement.text = html.unescape(statement.text)
+
+    return statement
+
+
+def convert_to_ascii(statement):
+    """
+    Converts unicode characters to ASCII character equivalents.
+    For example: "på fédéral" becomes "pa federal".
+    """
+    import unicodedata
+
+    text = unicodedata.normalize('NFKD', statement.text)
+    text = text.encode('ascii', 'ignore').decode('utf-8')
+
+    statement.text = str(text)
+    return statement
